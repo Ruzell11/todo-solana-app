3898cd4a02b813d67d5d1298b430b4320625c974
Ruzell11 <johnruzell123@gmail.com>
2024-06-29 10:28:13 +0800

    Add solana on the application

--- src/app/components/AddButton.tsx
+++ src/app/components/AddButton.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import { useTodo } from "../store/TodoProvider";
 import { TodoContextType } from "../common/type";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export default function
     AddButton() {
@@ -15,13 +16,17 @@ export default function
     }
 
     return (
-        <section className="flex gap-2">
-            <button className="text-white flex 
+        <section className="flex gap-2 justify-between items-center w-[100%]">
+            <div className="flex gap-2">
+                <button className="text-white flex 
         bg-slate-400 h-[40px] justify-center items-center w-100 p-5 rounded-md whitespace-nowrap"
-                onClick={() => handleSubmit()}>
-                Add To do
-            </button>
-            <input value={todoName} placeholder="Add todo" className="p-2 rounded-md bg-slate-300 w-[100%]" onChange={(e) => setTodoName(e.target.value)} />
+                    onClick={() => handleSubmit()}>
+                    Add To do
+                </button>
+                <input value={todoName} placeholder="Add todo" className="p-2 rounded-md bg-slate-300 w-[100%]" onChange={(e) => setTodoName(e.target.value)} required />
+            </div>
+
+            <WalletMultiButton style={{}} />
         </section>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@ import Image from "next/image";
 import AddButton from "./components/AddButton";
 import ListItem from "./components/ListItem";
 import TodoProvider from "./store/TodoProvider";
+import AppWalletProvider from "./components/AppWalletProvider";
 
 export default function Home() {
   return (
-    <TodoProvider>
-      <main className="flex flex-col items-start gap-5 justify-start w-[100%] p-8">
-        <AddButton />
-        <ListItem />
-      </main>
-    </TodoProvider>
+    <AppWalletProvider>
+      <TodoProvider>
+        <main className="flex flex-col items-start gap-5 justify-start w-[100%] p-8">
+          <AddButton />
+          <ListItem />
+        </main>
+      </TodoProvider>
+    </AppWalletProvider>
   );
 }
